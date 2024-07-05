c87c60f8586d0463ee4ed77b5757a91c10478c51
Lassi Chen <lachen@utu.fi>
2024-07-05 21:06:53 +0300

    init melee enemy

---
+++ Assets/Enemies/Melee.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+var direction = 1
+
+func _physics_process(delta):
+
+	if position.x > 1000:
+		direction ss= -1
+	velocity.x = direction * SPEED
+	if position.x < 0:
+		direction = 1
+	velocity.x = direction * SPEED
+
+	move_and_slide()
