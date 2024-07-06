c6b2dc4b4d608ae212ed5ad8ca08659c7c85deaf
Erno Lokkila <eolokk@utu.fi>
2024-07-06 17:19:40 +0300

    Player animation on farm

--- Assets/Player/Farm/Player_farm.gd
+++ Assets/Player/Farm/Player_farm.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 
 @export var carrot_seed : Seed
 var seed_inventory = []
+@onready var anim_player =  $Sprite2D
 
 func _ready():
 	seed_inventory.append(carrot_seed)
@@ -16,7 +17,13 @@ func _physics_process(_delta):
 	# Vertical movement input
 	var vertical = Input.get_axis("Farm_up", "Farm_down")
 	
+	if horizontal + vertical != 0:
+		anim_player.play("default")
+	else:
+		anim_player.stop()
+		
 	if horizontal:
+		anim_player.flip_h = false if horizontal > 0 else true
 		velocity.x = horizontal * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -25,7 +32,7 @@ func _physics_process(_delta):
 		velocity.y = vertical * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	
 	move_and_slide()
 
 func get_seeds():
