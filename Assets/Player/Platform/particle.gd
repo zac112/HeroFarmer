6b5d8eedd5e322d9d4cc18b70f2c194b0b5264d6
roopuli <roopejh@gmail.com>
2024-07-06 18:06:23 +0300

    Fix particle scene

--- Assets/Player/Platform/particle.gd
+++ Assets/Player/Platform/particle.gd
@@ -4,15 +4,14 @@ var speed = 750
 var direction = 1
 var damage = 10
 
+@export var bullet_particle : PackedScene
+
 func _physics_process(delta):
 	move_local_x(direction * speed * delta)
 
-
 func _on_body_entered(body):
+	var particle = bullet_particle.instantiate()
+	get_parent().add_child(particle)
 	if body.has_method("hit"):
 		body.hit(damage)
 	queue_free()
-
-
-func _ready():
-	$BulletAudio.play()
