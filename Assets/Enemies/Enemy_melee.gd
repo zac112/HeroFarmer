3cac9dd6f06c345ad4ae34333380863799b7086c
Lassi Chen <lachen@utu.fi>
2024-07-06 14:55:42 +0300

    Enemy moves to opposite direction when in edge

--- Assets/Enemies/Enemy_melee.gd
+++ Assets/Enemies/Enemy_melee.gd
@@ -8,14 +8,17 @@ var direction = 1
 
 @onready var ray_cast_left = $RayCastLeft
 @onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_down_left = $RayCastDownLeft
+@onready var ray_cast_down_right = $RayCastDownRight
 
 func _physics_process(delta):
-	if moving:
-		if ray_cast_right.is_colliding() or ray_cast_left.is_colliding():
+	if moving and is_on_floor():
+		if ray_cast_right.is_colliding() or ray_cast_left.is_colliding() or not ray_cast_down_left.is_colliding() or not ray_cast_down_right.is_colliding():
 			direction *= -1
 
 		velocity.x = direction * SPEED
-	velocity.y = GRAVITY
+	else:
+		velocity.y = GRAVITY
 
 	move_and_slide()
 
