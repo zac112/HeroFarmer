b63853b159371805c6cb9e6185cdd8312856a075
Lassi Chen <lachen@utu.fi>
2024-07-06 18:44:18 +0300

    Refactor enemy back

--- Assets/Enemies/Enemy_melee.gd
+++ Assets/Enemies/Enemy_melee.gd
@@ -4,6 +4,9 @@ const SPEED = 300
 const GRAVITY = 500
 var direction = 1
 
+@export var moving = true
+@export var following = false
+
 var health = 50
 
 @onready var ray_cast_left = $RayCastLeft
@@ -12,15 +15,20 @@ var health = 50
 @onready var ray_cast_down_right = $RayCastDownRight
 
 func _physics_process(delta):
-	if is_on_floor():
+	if moving and is_on_floor():
 		if ray_cast_right.is_colliding() or ray_cast_left.is_colliding() or not ray_cast_down_left.is_colliding() or not ray_cast_down_right.is_colliding():
 			direction *= -1
+
 		velocity.x = direction * SPEED
 	else:
 		velocity.y = GRAVITY
 
 	move_and_slide()
 
+
+func _on_player_detection_body_entered(body):
+	if body.name == "player_platform":
+		print("Player view area entered!")
 
 func hit(damage:int):
 	health -= damage
