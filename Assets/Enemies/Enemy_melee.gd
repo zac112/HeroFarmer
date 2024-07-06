23af73b0fb00fe9ce080d3ce149c11f5c5b967ba
Lassi Chen <lachen@utu.fi>
2024-07-06 12:41:58 +0300

    Enemy moves to opposite direction when colliding

--- Assets/Enemies/Enemy_melee.gd
+++ Assets/Enemies/Enemy_melee.gd
@@ -1,15 +1,25 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 300
+const GRAVITY = 500
 var direction = 1
 
-func _physics_process(delta):
+@export var moving = true
 
-	if position.x > 1000:
-		direction = -1
-	velocity.x = direction * SPEED
-	if position.x < 0:
-		direction = 1
-	velocity.x = direction * SPEED
+@onready var ray_cast_left = $RayCastLeft
+@onready var ray_cast_right = $RayCastRight
+
+func _physics_process(delta):
+	if moving:
+		if ray_cast_right.is_colliding() or ray_cast_left.is_colliding():
+			direction *= -1
+
+		velocity.x = direction * SPEED
+	velocity.y = GRAVITY
 
 	move_and_slide()
+
+
+func _on_player_detection_body_entered(body):
+	if body.name == "player_platform":
+		print("Player view area entered!")
