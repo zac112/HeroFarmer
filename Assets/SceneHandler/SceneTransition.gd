9925e988c45a7d501a51d39a2cf0524ed1acb91b
Erno Lokkila <eolokk@utu.fi>
2024-07-07 10:22:47 +0300

    Scene transition now work with ints. Moved Level 1 to folder

--- Assets/SceneHandler/SceneTransition.gd
+++ Assets/SceneHandler/SceneTransition.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
-@export var targetScene : String
+var levels = ["res://Assets/Farm/Farm.tscn",
+"res://Assets/Platform/Level1/level_1.1.tscn",
+"res://Assets/Platform/Boss/level_final.tscn"]
+
+@export_range(0,3) var targetScene : int
 
 func _on_body_entered(body):
 	SceneHandler.loadScene(targetScene)
