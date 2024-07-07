15052e2a0abc07dd626509079bc336dcb1ab9eed
roopuli <roopejh@gmail.com>
2024-07-07 18:10:08 +0300

    Plot improvements
    - Added pumpkin
    - Harvest now adds to crop inventory

--- Assets/Inventory/seed_inventory/seed_inventory.gd
+++ Assets/Inventory/seed_inventory/seed_inventory.gd
@@ -2,7 +2,8 @@ extends Node
 
 var carrot_seed : SeedItem = load("res://Assets/Inventory/seed_inventory/carrot_seed.tres")
 var turnip_seed : SeedItem = load("res://Assets/Inventory/seed_inventory/turnip_seed.tres")
-var inventory = [carrot_seed, turnip_seed] 
+var pumpkin_seed : SeedItem = load("res://Assets/Inventory/seed_inventory/pumpkin_seed.tres")
+var inventory = [carrot_seed, turnip_seed, pumpkin_seed] 
 
 func pickup_seed(seed_id : int):
 	inventory[seed_id].quantity += 1
